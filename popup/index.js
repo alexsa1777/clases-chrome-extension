df407d977be9d2843a26e73e21be35bad082a3b3
alexsa1777 <alexsa.1kk7@gmail.com>
2023-04-07 19:04:11 -0500

    save changes

--- popup/index.js
+++ popup/index.js
@@ -1,43 +1,53 @@
-const btnScripting = document.getElementById("btnscript");
+const btnScripting = document.getElementById("btncomunicacion");
+const btnScriptingBackground = document.getElementById("btncomunicacionbckg");
 
 btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: alertHelloWorld,
+  let port = chrome.tabs.connect(tab.id, { name: "popup" });
+  port.postMessage({ message: "hola" });
+  port.onMessage.addListener(function ({ message }) {
+    alert(message);
   });
 });
 
-function alertHelloWorld() {
-  // alert("Hello World");
-  let getJobs = [...document.querySelectorAll('[id*="jobcard-"]')]
-  const jobs = getJobs.map((jobcard) => {
-    const [
-      { href: url },
-      {
-        children: [
-          {
-            children: [
-              { innerText: date },
-              { innerText: title },
-              { innerText: rangeSalary },
-              { innerText: benefits },
-              { innerText: description },
-              { innerText: company },
-            ],
-          },
-        ],
-      },
-    ] = jobcard.children;
-    return {
-      date: date.split("\n")[0],
-      title,
-      rangeSalary,
-      benefits: benefits.split("\n"),
-      description,
-      company: company.split("\n")[0],
-      url,
-    };
+btnScriptingBackground.addEventListener("click", async () => {
+  var port = chrome.runtime.connect({ name: "popup-background" });
+  port.postMessage({ message: "Hola BD" });
+  port.onMessage.addListener(function ({ message }) {
+    alert(message);
   });
-  console.log(jobs);
-};
+});
+
+// function alertHelloWorld() {
+//   // alert("Hello World");
+//   let getJobs = [...document.querySelectorAll('[id*="jobcard-"]')]
+//   const jobs = getJobs.map((jobcard) => {
+//     const [
+//       { href: url },
+//       {
+//         children: [
+//           {
+//             children: [
+//               { innerText: date },
+//               { innerText: title },
+//               { innerText: rangeSalary },
+//               { innerText: benefits },
+//               { innerText: description },
+//               { innerText: company },
+//             ],
+//           },
+//         ],
+//       },
+//     ] = jobcard.children;
+//     return {
+//       date: date.split("\n")[0],
+//       title,
+//       rangeSalary,
+//       benefits: benefits.split("\n"),
+//       description,
+//       company: company.split("\n")[0],
+//       url,
+//     };
+//   });
+//   console.log(jobs);
+// };
